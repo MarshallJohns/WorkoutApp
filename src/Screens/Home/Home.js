b2d0abd05c605eed4930efb9e7563c54b1565ccc
MarshallJohns <marshalljohnson193@gmail.com>
2024-03-07 20:03:55 -0700

    inital commit for routing

---
+++ src/Screens/Home/Home.js
@@ -0,0 +1,25 @@
+//React & React Native
+import React, {useContext, useEffect, useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+//Packages
+//Context
+//Constants
+//Navigation
+//Components
+//Screens
+//Icons
+//Images
+//Data
+//Styles
+
+export const Home = () => {
+  return (
+    <View style={styles.content}>
+      <Text>Home.js</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  content: {},
+});
